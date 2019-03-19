a9e224cc8b8d76f3d59aa48ca9724565a2e07ef5
Adam <adam.and.math@gmail.com>
2019-03-19 15:09:22 +0100

    implimented a complete turing machine

---
+++ src/main.rs
@@ -0,0 +1,324 @@
+#[macro_use]
+mod turing;
+
+use std::thread::sleep;
+use std::time::Duration;
+use turing::State;
+
+fn main() {
+    let mut args = ::std::env::args().skip(1);
+    let machine = args.next().unwrap_or("copy".to_owned());
+    let input = args.next();
+    let input = input.as_ref().map(|s|s.as_ref());
+
+    match machine.as_ref() {
+        "copy" => copy(input),
+        "calc" => calcuator(input),
+        m => println!("Unknown machine: {}", m),
+    }
+}
+
+fn copy(input: Option<&str>) {
+    let input = input.unwrap_or("_abbaab");
+    let t = turing! ('_';9;
+        (0) { '_' => (Right, '_', 1) },
+        (1) {
+            'a' => (Right, 'A', 2)
+            'b' => (Right, 'B', 4)
+            '_' => (Left , '_', 8)
+        },
+        (2) {
+            'a' => (Right, 'a', 2)
+            'b' => (Right, 'b', 2)
+            '_' => (Right, '_', 3)
+        },
+        (3) {
+            'a' => (Right, 'a', 3)
+            'b' => (Right, 'b', 3)
+            '_' => (Left , 'a', 6)
+        },
+        (4) {
+            'a' => (Right, 'a', 4)
+            'b' => (Right, 'b', 4)
+            '_' => (Right, '_', 5)
+        },
+        (5) {
+            'a' => (Right, 'a', 5)
+            'b' => (Right, 'b', 5)
+            '_' => (Left , 'b', 6)
+        },
+        (6) {
+            'a' => (Left , 'a', 6)
+            'b' => (Left , 'b', 6)
+            '_' => (Left , '_', 7)
+        },
+        (7) {
+            'a' => (Left , 'a', 7)
+            'b' => (Left , 'b', 7)
+            'A' => (Right, 'A', 1)
+            'B' => (Right, 'B', 1)
+        },
+        (8) {
+            'A' => (Left , 'a', 8)
+            'B' => (Left , 'b', 8)
+            '_' => (Stay , '_', 9)
+        },
+    );
+
+    let state: State<char, usize> = State::new(input.chars().collect(), 0);
+    match t.debug(state, |s| {println!("{}", s); sleep(Duration::from_millis(500))}) {
+        None => println!("Failed"),
+        Some(state) => {
+            println!("Finished as");
+            println!("{}", state)
+        }
+    }
+}
+
+fn calcuator(input: Option<&str>) {
+    let input = input.unwrap_or("_uucz1100,0101");
+    let t = turing! ('_';"Done";
+        ("Start") { '_' => (Right, '_', "Start2") },
+        ("Start2") {
+            '0' => (Right, '0', "Start2")
+            '1' => (Right, '1', "Start2")
+            ',' => (Right, ',', "Start2")
+            ' ' => (Right, ' ', "Start2")
+            '!' => (Right, '!', "Start2")
+            'z' => (Right, 'z', "Start2")
+            'u' => (Right, 'u', "Start2")
+            'c' => (Right, 'c', "Start2")
+            's' => (Right, 's', "Start2")
+            '|' => (Right, '|', "Start2")
+            '&' => (Right, '&', "Start2")
+            '+' => (Right, '+', "Start2")
+            '-' => (Right, '-', "Start2")
+            '_' => (Left , '_', "Main")
+        },
+        ("Main") {
+            '0' => (Left , '0', "Main")
+            '1' => (Left , '1', "Main")
+            ',' => (Left , ',', "Main")
+            '_' => (Stay , '_', "Done")
+            ' ' => (Right, ' ', "Space")
+            '!' => (Right, ' ', "Not")
+            'z' => (Right, ' ', "Zip")
+            'u' => (Right, ',', "Unzip")
+            'c' => (Right, ' ', "Copy")
+            's' => (Right, ' ', "Swap")
+            '|' => (Right, ' ', "Or")
+            '&' => (Right, ' ', "And")
+            '+' => (Right, ' ', "Add")
+        },
+        ("Space") {
+            '0' => (Right, '0', "Space")
+            '1' => (Right, '1', "Space")
+            ',' => (Right, ',', "Space")
+            '_' => (Left , '_', "Space:_")
+        },
+        ("Space:_") {
+            '0' => (Left, '_', "Space:0")
+            '1' => (Left, '_', "Space:1")
+            ',' => (Left, '_', "Space:,")
+            ' ' => (Left, '_', "Main")
+        },
+        ("Space:0") {
+            '0' => (Left, '0', "Space:0")
+            '1' => (Left, '0', "Space:1")
+            ',' => (Left, '0', "Space:,")
+            ' ' => (Left, '0', "Main")
+        },
+        ("Space:1") {
+            '0' => (Left, '1', "Space:0")
+            '1' => (Left, '1', "Space:1")
+            ',' => (Left, '1', "Space:,")
+            ' ' => (Left, '1', "Main")
+        },
+        ("Space:,") {
+            '0' => (Left, ',', "Space:0")
+            '1' => (Left, ',', "Space:1")
+            ',' => (Left, ',', "Space:,")
+            ' ' => (Left, ',', "Main")
+        },
+        ("Zip") {
+            '0' => (Right, '0', "Zip2")
+            '1' => (Right, '1', "Zip2")
+        },
+        ("Zip2") {
+            '0' => (Right, ' ', "Zip2:0")
+            '1' => (Right, ' ', "Zip2:1")
+            ',' => (Right, ' ', "Main")
+        },
+        ("Zip2:0") {
+            '0' => (Right, '0', "Zip2:0")
+            '1' => (Right, '0', "Zip2:1")
+            ',' => (Right, '0', "Zip3")
+        },
+        ("Zip2:1") {
+            '0' => (Right, '1', "Zip2:0")
+            '1' => (Right, '1', "Zip2:1")
+            ',' => (Right, '1', "Zip3")
+        },
+        ("Zip3") {
+            '0' => (Left , ',', "Zip3:0")
+            '1' => (Left , ',', "Zip3:1")
+        },
+        ("Zip3:0") {
+            '0' => (Left , '0', "Zip3:0")
+            '1' => (Left , '1', "Zip3:0")
+            ' ' => (Right, '0', "Zip")
+        },
+        ("Zip3:1") {
+            '0' => (Left , '0', "Zip3:1")
+            '1' => (Left , '1', "Zip3:1")
+            ' ' => (Right, '1', "Zip")
+        },
+        ("Unzip") {
+            '0' => (Left , ' ', "Unzip:0")
+            '1' => (Left , ' ', "Unzip:1")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("Unzip:0") {
+            '0' => (Left , '0', "Unzip:0")
+            '1' => (Left , '1', "Unzip:0")
+            ',' => (Right, '0', "Unzip2")
+        },
+        ("Unzip:1") {
+            '0' => (Left , '0', "Unzip:1")
+            '1' => (Left , '1', "Unzip:1")
+            ',' => (Right, '1', "Unzip2")
+        },
+        ("Unzip2") {
+            '0' => (Right, ',', "Unzip2:0")
+            '1' => (Right, ',', "Unzip2:1")
+            ' ' => (Right, ',', "Unzip3")
+        },
+        ("Unzip2:0") {
+            '0' => (Right, '0', "Unzip2:0")
+            '1' => (Right, '0', "Unzip2:1")
+            ' ' => (Right, '0', "Unzip3")
+        },
+        ("Unzip2:1") {
+            '0' => (Right, '1', "Unzip2:0")
+            '1' => (Right, '1', "Unzip2:1")
+            ' ' => (Right, '1', "Unzip3")
+        },
+        ("Unzip3") {
+            '0' => (Right, '0', "Unzip")
+            '1' => (Right, '1', "Unzip")
+        },
+        ("Copy") {
+            '0' => (Left, '0', "Copy:0")
+            '1' => (Left, '1', "Copy:1")
+        },
+        ("Copy:0") { ' ' => (Right, '0', "Copy2") },
+        ("Copy:1") { ' ' => (Right, '1', "Copy2") },
+        ("Copy2") {
+            '0' => (Right, '0', "Copy3")
+            '1' => (Right, '1', "Copy3")
+        },
+        ("Copy3") {
+            '0' => (Right, 'c', "Insert:0")
+            '1' => (Right, 'c', "Insert:1")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("Insert:0") {
+            '0' => (Right, '0', "Insert:0")
+            '1' => (Right, '0', "Insert:1")
+            ',' => (Right, '0', "Insert:,")
+            '_' => (Left , '0', "Main")
+        },
+        ("Insert:1") {
+            '0' => (Right, '1', "Insert:0")
+            '1' => (Right, '1', "Insert:1")
+            ',' => (Right, '1', "Insert:,")
+            '_' => (Left , '1', "Main")
+        },
+        ("Insert:,") {
+            '0' => (Right, ',', "Insert:0")
+            '1' => (Right, ',', "Insert:1")
+            ',' => (Right, ',', "Insert:,")
+        },
+        ("Swap") {
+            '0' => (Right, ',', "Swap:0")
+            '1' => (Right, ',', "Swap:1")
+        },
+        ("Swap:0") {
+            '0' => (Right, '0', "Swap:0")
+            '1' => (Right, '0', "Swap:1")
+            ',' => (Right, '0', "Swap2")
+        },
+        ("Swap:1") {
+            '0' => (Right, '1', "Swap:0")
+            '1' => (Right, '1', "Swap:1")
+            ',' => (Right, '1', "Swap2")
+        },
+        ("Swap2") {
+            '0' => (Left , ',', "Swap2:0")
+            '1' => (Left , ',', "Swap2:1")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("Swap2:0") {
+            '0' => (Left , '0', "Swap2:0")
+            '1' => (Left , '1', "Swap2:0")
+            ',' => (Right, '0', "Swap")
+        },
+        ("Swap2:1") {
+            '0' => (Left , '0', "Swap2:1")
+            '1' => (Left , '1', "Swap2:1")
+            ',' => (Right, '1', "Swap")
+        },
+        ("Not") {
+            '0' => (Right, '1', "Not")
+            '1' => (Right, '0', "Not")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("Or") {
+            '0' => (Right, ' ', "Or:0")
+            '1' => (Right, ' ', "Or:1")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("Or:0") {
+            '0' => (Right, '0', "Or")
+            '1' => (Right, '1', "Or")
+        },
+        ("Or:1") {
+            '0' => (Right, '1', "Or")
+            '1' => (Right, '1', "Or")
+        },
+        ("And") {
+            '0' => (Right, ' ', "And:0")
+            '1' => (Right, ' ', "And:1")
+            ',' => (Left , ',', "Main")
+            '_' => (Left , '_', "Main")
+        },
+        ("And:0") {
+            '0' => (Right, '0', "And")
+            '1' => (Right, '0', "And")
+        },
+        ("And:1") {
+            '0' => (Right, '1', "And")
+            '1' => (Right, '1', "And")
+        },
+        ("Add") {
+            '0' => (Right, '0', "Add")
+            '1' => (Right, '1', "Add")
+            ',' => (Left , ',', "Add2")
+            '_' => (Left , '_', "Add2")
+        },
+    );
+
+    let state: State<char, &str> = State::new(input.chars().collect(), "Start");
+    match t.debug(state, |s| {println!("{}", s); sleep(Duration::from_millis(100))}) {
+        None => println!("Failed"),
+        Some(state) => {
+            println!("Finished as");
+            println!("{}", state)
+        }
+    }
+}
---
+++ src/turing.rs
@@ -0,0 +1,118 @@
+use std::collections::{ VecDeque, HashMap };
+use std::fmt::{ self, Display, Formatter };
+use std::hash::Hash;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Dir {
+    Left,
+    Stay,
+    Right,
+}
+
+pub struct State<Sym, Mem> {
+    tape: VecDeque<Sym>,
+    pos: usize,
+    mem: Mem,
+}
+
+impl<Sym, Mem> State<Sym, Mem> {
+    pub fn new(tape: VecDeque<Sym>, mem: Mem) -> Self {
+        State { tape, mem, pos: 0 }
+    }
+
+    pub fn current(&self) -> &Sym {
+        &self.tape[self.pos]
+    }
+
+    fn write(&mut self, sym: Sym) {
+        self.tape[self.pos] = sym;
+    }
+
+    fn mov(&mut self, dir: Dir, def: &Sym) where Sym: Clone {
+        match dir {
+            Dir::Left if self.pos == 0 => self.tape.push_front(def.clone()),
+            Dir::Left => self.pos -= 1,
+            Dir::Stay => (),
+            Dir::Right => {
+                self.pos += 1;
+                if self.pos == self.tape.len() {
+                    self.tape.push_back(def.clone())
+                }
+            }
+        }
+    }
+
+    pub fn print(&self, view: usize) where Sym: Display, Mem: Display {
+        let min = if self.pos < view { 0 } else { self.pos - view };
+        let max = self.tape.len().max(self.pos + view + 1);
+        println!("mem: {}", self.mem);
+        for sym in self.tape.iter().take(max).skip(min) {
+            print!("{}", sym);
+        }
+        println!();
+        println!("{:>1$}", "^", self.pos - min + 1);
+    }
+}
+
+impl<Sym: Display, Mem: Display> Display for State<Sym, Mem> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        writeln!(f, "mem: {}", self.mem)?;
+        for sym in &self.tape {
+            write!(f, "{}", sym)?;
+        }
+
+        writeln!(f, "")?;
+        write!(f, "{:>1$}", "^", self.pos+1)
+    }
+}
+
+pub struct Turing<Sym, Mem> {
+    map: HashMap<(Sym, Mem), (Dir, Sym, Mem)>,
+    default: Sym,
+    accepted: Mem,
+}
+
+impl<Sym, Mem> Turing<Sym, Mem> {
+    pub fn new(map: HashMap<(Sym, Mem), (Dir, Sym, Mem)>, default: Sym, accepted: Mem) -> Self {
+        Turing { map, default, accepted }
+    }
+
+    fn step(&self, mut state: State<Sym, Mem>) -> Option<State<Sym, Mem>>
+        where Sym: Clone + Eq + Hash + Default, Mem: Clone + Eq + Hash {
+        let (dir, sym, mem) = self.map.get(&(state.current().clone(), state.mem.clone()))?;
+        state.write(sym.clone());
+        state.mem = mem.clone();
+        state.mov(*dir, &self.default);
+        Some(state)
+    }
+
+    pub fn run(&self, mut state: State<Sym, Mem>) -> Option<State<Sym, Mem>>
+        where Sym: Clone + Eq + Hash + Default, Mem: Clone + Eq + Hash {
+        while state.mem != self.accepted {
+            state = self.step(state)?;
+        }
+        Some(state)
+    }
+
+    pub fn debug<F: Fn(&State<Sym, Mem>)>(&self, mut state: State<Sym, Mem>, peek: F) -> Option<State<Sym, Mem>>
+        where Sym: Clone + Eq + Hash + Default + Display, Mem: Clone + Eq + Hash + Display {
+        while state.mem != self.accepted {
+            peek(&state);
+            state = self.step(state)?;
+        }
+        Some(state)
+    }
+}
+
+macro_rules! turing {
+    ($default:expr ; $accept:expr ; $(($mem:expr) { $($sym:expr => ($dir:ident, $sym_new:expr, $mem_new:expr))+ },)+) => (
+        turing!($default ; $accept ; $(($mem) { $($sym => ($dir, $sym_new, $mem_new))+ }),+)
+    );
+    ($default:expr ; $accept:expr ; $(($mem:expr) { $($sym:expr => ($dir:ident, $sym_new:expr, $mem_new:expr))+ }),+) => ({
+        let mut map = std::collections::HashMap::new();
+        $($(
+            map.insert(($sym, $mem), (crate::turing::Dir::$dir, $sym_new, $mem_new));
+        )+)+
+        crate::turing::Turing::new(map, $default, $accept)
+    })
+}
