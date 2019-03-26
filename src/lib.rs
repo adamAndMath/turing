72ea4510a2b760603d8b67185517e9d4146b6a33
Adam <adam.and.math@gmail.com>
2019-03-26 08:17:39 +0100

    Implementing space for vec of space

--- src/lib.rs
+++ src/lib.rs
@@ -132,6 +132,23 @@ impl_tuple_space!(0:T0,1:T1,2:T2,3:T3,4:T4,5:T5,6:T6,7:T7,8:T8);
 impl_tuple_space!(0:T0,1:T1,2:T2,3:T3,4:T4,5:T5,6:T6,7:T7,8:T8,9:T9);
 impl_array_space!(32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0);
 
+impl<T: Space> Space for Vec<T> {
+    type Sym = Vec<T::Sym>;
+    type Dir = Vec<T::Dir>;
+
+    fn read(&self) -> Self::Sym {
+        self.iter().map(|s|s.read()).collect()
+    }
+
+    fn write(&mut self, sym: Self::Sym) {
+        self.iter_mut().zip(sym).for_each(|(t,s)|t.write(s));
+    }
+
+    fn mov(&mut self, dir: Self::Dir, def: &Self::Sym) {
+        self.iter_mut().zip(dir).zip(def).for_each(|((t,d),s)|t.mov(d,s));
+    }
+}
+
 pub struct Turing<S: Space, Mem> {
     map: HashMap<(S::Sym, Mem), (S::Dir, S::Sym, Mem)>,
     default: S::Sym,
